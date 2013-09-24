9190f96d8700e98883c84ce7876cca98349d3b6b
sgr <sgr0502@gmail.com>
2013-09-24 18:19:35 +0900

    0.3.4 released

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject desktop-alert "0.3.3"
+(defproject desktop-alert "0.3.4"
   :description "A Clojure library designed for your application to add desktop alert easily."
   :url "https://github.com/sgr/desktop-alert"
   :license {:name "Eclipse Public License"
